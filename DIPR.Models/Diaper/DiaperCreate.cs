4ce684334d7deceed55de365b2badec40bc9f1d2
Max Sigmon <max.j.sigmon@gmail.com>
2020-12-14 15:49:52 -0500

    added diaper models

---
+++ DIPR.Models/Diaper/DiaperCreate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DIPR.Models.Diaper
+{
+    public class DiaperCreate
+    {
+        public DiaperCreate()
+        {
+            SoiledList = GetSoiledSelectList();
+        }
+
+        public SelectList SoiledList { get; set; }
+        public Soiled Soiled { get; set; }
+        public static SelectList GetSoiledSelectList()
+        {
+            var enumValues = Enum.GetValues(typeof(Soiled)).Cast<Soiled>().Select(e => new { Value = e.ToString(), Text = e.ToString() }).ToList();
+            return new SelectList(enumValues, "Value", "Text", "");
+        }
+    }
+}
---
+++ DIPR.Models/Diaper/DiaperListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPR.Models.Diaper
+{
+    public class DiaperListItem
+    {
+        public Soiled Soiled { get; set; }
+    }
+
+    public enum Soiled
+    {
+        Wet,
+        Dirty,
+        Both
+    }
+}
