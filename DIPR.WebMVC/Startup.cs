b4c8ba07d8cf08ead1eb4154d440f58fe77e36b8
Max Sigmon <max.j.sigmon@gmail.com>
2020-12-14 10:26:58 -0500

    Add project files.

---
+++ DIPR.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DIPR.WebMVC.Startup))]
+namespace DIPR.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
