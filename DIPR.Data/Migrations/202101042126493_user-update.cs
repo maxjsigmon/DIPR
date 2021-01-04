fadabeb18abb44d9bb7e3eecba26a56639adbe9e
Max Sigmon <max.j.sigmon@gmail.com>
2021-01-04 17:22:13 -0500

    home layout update

---
+++ DIPR.Data/Migrations/202101042126493_user-update.cs
@@ -0,0 +1,16 @@
+namespace DIPR.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userupdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
